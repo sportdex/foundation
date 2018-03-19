f59e8a40b4fdc806189948095321cfb3271a5d7f
Lev <wucy@sportik.me>
2018-03-19 15:02:13 -0700

    Add test for SportikToken on delegates

--- test/token/DelegatableTokenTest.js
+++ test/token/DelegatableTokenTest.js
@@ -2,6 +2,7 @@ import utils from 'ethereumjs-util';
 import web3Utils from 'web3-utils';
 
 const DelegatableTokenMock = artifacts.require("DelegatableTokenMock");
+const SportikToken = artifacts.require("SportikToken");
 
 // The hardcoded private key for accounts[0] in testrpc, used for signing
 const PRIVATE_KEY = Buffer.from('c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', 'hex');
@@ -27,83 +28,108 @@ function assertEvent(event, checker) {
   });
 }
 
+async function assertFailure(p) {
+  try {
+    if (typeof p === 'function') {
+      p = p();
+    }
+    await p;
+    assert.fail();
+  } catch (e) {}
+}
+
+async function testDelegateTransfer(token, accounts) {
+  const supply = (await token.totalSupply()).toNumber();
+  const from = accounts[0];
+  const delegate = accounts[1];
+  const to = accounts[2];
+  const amount = supply / 2;
+  const nonce = 100;
+  assert.equal(await token.balanceOf(from), supply);
+
+  let { v, r, s } = signMethodCall(
+    delegate, token.address,
+    'delegatedTransfer', [
+      { type: 'address', value: from },
+      { type: 'address', value: to },
+      { type: 'uint256', value: amount },
+      { type: 'uint256', value: nonce },
+    ],
+  );
+  await token.delegatedTransfer(from, to, amount, nonce, v, r, s, { from: delegate });
+
+  assertEvent(token.DelegatedTransfer(), logs => {
+    const event = logs[0].args;
+    assert.equal(event.delegate, delegate);
+    assert.equal(event.from, from);
+    assert.equal(event.to, to);
+    assert.equal(event.amount, amount);
+  });
+  assert.equal(await token.balanceOf(from), amount);
+  assert.equal(await token.balanceOf(to), amount);
+
+  await assertFailure(token.delegatedTransfer(from, to, amount, nonce, v, r, s, { from: delegate }));
+  assert.equal(await token.balanceOf(from), amount);
+  assert.equal(await token.balanceOf(to), amount);
+}
+
+async function testDelegateApprove(token, accounts) {
+  const supply = (await token.totalSupply()).toNumber();
+  const owner = accounts[0];
+  const delegate = accounts[1];
+  const spender = accounts[2];
+  const amount = supply / 2;
+  const nonce = 100;
+
+  let { v, r, s } = signMethodCall(
+    delegate, token.address,
+    'delegatedApprove', [
+      { type: 'address', value: owner },
+      { type: 'address', value: spender },
+      { type: 'uint256', value: amount },
+      { type: 'uint256', value: nonce },
+    ],
+  );
+  await token.delegatedApprove(owner, spender, amount, nonce, v, r, s, { from: delegate });
+
+  assertEvent(token.DelegatedApprove(), logs => {
+    const event = logs[0].args;
+    assert.equal(event.delegate, delegate);
+    assert.equal(event.owner, owner);
+    assert.equal(event.spender, spender);
+    assert.equal(event.amount, amount);
+  });
+  assert.equal(await token.allowance(owner, spender), amount);
+
+  const receiver = accounts[3];
+  await token.transferFrom(owner, receiver, amount / 4, { from: spender });
+  assert.equal(await token.allowance(owner, spender), amount * 3 / 4);
+  assert.equal(await token.balanceOf(owner), supply * 7 / 8);
+  assert.equal(await token.balanceOf(receiver), amount / 4);
+}
+
 contract('DelegatableToken', accounts => {
-  const supply = 100000;
-  let token;
-
-  beforeEach(async () => {
-    token = await DelegatableTokenMock.new(supply);
-  });
-
   it('should delegate transfer', async () => {
-    const from = accounts[0];
-    const delegate = accounts[1];
-    const to = accounts[2];
-    const amount = supply / 2;
-    const nonce = 100;
-    assert.equal(await token.balanceOf(from), supply);
-
-    let { v, r, s } = signMethodCall(
-      delegate, token.address,
-      'delegatedTransfer', [
-        { type: 'address', value: from },
-        { type: 'address', value: to },
-        { type: 'uint256', value: amount },
-        { type: 'uint256', value: nonce },
-      ],
-    );
-    await token.delegatedTransfer(from, to, amount, nonce, v, r, s, { from: delegate });
-
-    assertEvent(token.DelegatedTransfer(), logs => {
-      const event = logs[0].args;
-      assert.equal(event.delegate, delegate);
-      assert.equal(event.from, from);
-      assert.equal(event.to, to);
-      assert.equal(event.amount, amount);
-    });
-    assert.equal(await token.balanceOf(from), amount);
-    assert.equal(await token.balanceOf(to), amount);
-
-    try {
-      await token.delegatedTransfer(from, to, amount, nonce, v, r, s, { from: delegate });
-      assert.fail();
-    } catch (e) {}
-    assert.equal(await token.balanceOf(from), amount);
-    assert.equal(await token.balanceOf(to), amount);
+    const token = await DelegatableTokenMock.new(100000);
+    await testDelegateTransfer(token, accounts);
   });
-
   it('should delegate approve', async () => {
-    const owner = accounts[0];
-    const delegate = accounts[1];
-    const spender = accounts[2];
-    const amount = supply / 2;
-    const nonce = 100;
-    assert.equal(await token.balanceOf(owner), supply);
+    const token = await DelegatableTokenMock.new(100000);
+    await testDelegateApprove(token, accounts);
+  });
+});
 
-    let { v, r, s } = signMethodCall(
-      delegate, token.address,
-      'delegatedApprove', [
-        { type: 'address', value: owner },
-        { type: 'address', value: spender },
-        { type: 'uint256', value: amount },
-        { type: 'uint256', value: nonce },
-      ],
-    );
-    await token.delegatedApprove(owner, spender, amount, nonce, v, r, s, { from: delegate });
-
-    assertEvent(token.DelegatedApprove(), logs => {
-      const event = logs[0].args;
-      assert.equal(event.delegate, delegate);
-      assert.equal(event.owner, owner);
-      assert.equal(event.spender, spender);
-      assert.equal(event.amount, amount);
-    });
-    assert.equal(await token.allowance(owner, spender), amount);
-
-    const receiver = accounts[3];
-    await token.transferFrom(owner, receiver, amount / 4, { from: spender });
-    assert.equal(await token.allowance(owner, spender), amount * 3 / 4);
-    assert.equal(await token.balanceOf(owner), supply * 7 / 8);
-    assert.equal(await token.balanceOf(receiver), amount / 4);
+contract('SportikToken', accounts => {
+  it('should delegate transfer', async () => {
+    const token = await SportikToken.new();
+    await assertFailure(() => testDelegateTransfer(token, accounts));
+    await token.addDelegate(accounts[1]);
+    await testDelegateTransfer(token, accounts);
+  });
+  it('should delegate approve', async () => {
+    const token = await SportikToken.new();
+    await assertFailure(() => testDelegateApprove(token, accounts));
+    await token.addDelegate(accounts[1]);
+    await testDelegateApprove(token, accounts);
   });
 });
